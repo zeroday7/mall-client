340da059ffbf35c8c1f367922bf13055a5423d90
zeroday7 <jjdev@naver.com>
2021-04-21 12:02:40 +0900

    오타수정

--- mall_client/src/mall/client/listener/StatsListener.java
+++ mall_client/src/mall/client/listener/StatsListener.java
@@ -14,13 +14,16 @@ public class StatsListener implements HttpSessionListener {
     public void sessionCreated(HttpSessionEvent se)  { 
     	// 1. 오늘날짜의 카운트가 없으면 1을 입력
     	// 2. 오늘날짜의 카운트가 있으면 +1을 업데이트
-    	System.out.println("새로운 세션이 생성 되었습니다.");
     	
-    	this.statsDao = new StatsDao();
-    	if(this.statsDao.selectStatsByToday() == null) {
-    		this.statsDao.insertStats();
-    	} else {
-    		this.statsDao.updateStats();
+    	if(se.getSession().isNew()) {
+	    	System.out.println("새로운 세션이 생성 되었습니다.");
+	    	
+	    	this.statsDao = new StatsDao();
+	    	if(this.statsDao.selectStatsByToday() == null) {
+	    		this.statsDao.insertStats();
+	    	} else {
+	    		this.statsDao.updateStats();
+	    	}
     	}
     }
 
